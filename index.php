19d9757497946fdcc6d81a0158c8d758b530fba2
arturemildossantos <artur.emil@gmail.com>
2020-12-10 15:14:55 -0300

    segundo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Aprender sempre</title>
     </head>
 <body background = "img104.jpg">
-    <p style="font-size:70px;color:blue;text-align:center">Hoje um pouco de CD\CI</p>
+    <p style="font-size:70px;color:red;text-align:center">Hoje um pouco de CD\CI</p>
 
 </body>
 </html>
