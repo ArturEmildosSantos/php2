05bdf7033074a1530e3337899fa0dbece629eae7
arturemildossantos <artur.emil@gmail.com>
2020-12-10 15:03:37 -0300

    segundo

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Aprender sempre</title>
+    </head>
+<body background = "img7.jpg">
+    <p style="font-size:70px;color:blue;text-align:center">Hoje um pouco de CD\CI</p>
+
+</body>
+</html>
